446d86e9850ff857f5f4fbd1ae925b6a40b9ac20
Iftekher Aziz <azizchy02@gmail.com>
2023-05-24 01:47:11 +0600

    Project setup

---
+++ bistro-boss-client/src/pages/Home/Home/Home.jsx
@@ -0,0 +1,26 @@
+import Banner from "../Banner/Banner";
+import CallUs from "../CallUs/CallUs";
+import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
+import PopularMenu from "../PopularMenu/PopularMenu";
+import Products from "../Products/Products";
+import Review from "../Review/Review";
+import Testimonial from "../Testimonial/Testimonial";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <Category></Category>
+      <Testimonial></Testimonial>
+      <PopularMenu></PopularMenu>
+      <CallUs></CallUs>
+      <Products></Products>
+      <Featured></Featured>
+      <Review></Review>
+    </div>
+  );
+};
+
+export default Home;
