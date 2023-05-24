ff15d6306f9a87982feaca6f97b050d5e31adaed
Iftekher Aziz <azizchy02@gmail.com>
2023-05-24 23:32:53 +0600

    Display different menu category items with cover

--- bistro-boss-client/src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ bistro-boss-client/src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -2,8 +2,23 @@
 // import Cover from "../../Shared/Cover/Cover";
 // import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
-  return <div></div>;
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+const MenuCategory = ({items}) => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-10 mt-20">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="text-center my-10">
+        <button className="btn btn-outline border-0 border-b-4 mt-4 uppercase">
+          Order Your Favourite Food
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
