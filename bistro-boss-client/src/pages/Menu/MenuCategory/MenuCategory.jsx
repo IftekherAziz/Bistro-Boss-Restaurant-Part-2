5086b998f42764a6898f1896c9aa693c91832066
Iftekher Aziz <azizchy02@gmail.com>
2023-05-24 22:47:18 +0600

    Create Custom Hook Usemenu To Load Menu Data

--- bistro-boss-client/src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ bistro-boss-client/src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
-import Cover from "../../Shared/Cover/Cover";
-import MenuItem from "../../Shared/MenuItem/MenuItem";
+// import { Link } from "react-router-dom";
+// import Cover from "../../Shared/Cover/Cover";
+// import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const MenuCategory = () => {
   return <div></div>;
